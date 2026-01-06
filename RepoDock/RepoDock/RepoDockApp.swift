b1366ce328c030d9c6067f24339dc8ce841bf6d1
Armaan Jhanji <armaanj32@gmail.com>
2026-01-06 19:43:45 +0400

    chore: initialise swiftui app

---
+++ RepoDock/RepoDock/RepoDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoDockApp.swift
+//  RepoDock
+//
+//  Created by Armaan Jhanji on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct RepoDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
